fe0b8c280993c183264fa2077116b48487b26a33
huzaifa-ship-it <mr.gulu.00001@gmail.com>
2025-07-24 21:12:40 +0500

    update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -64,7 +64,7 @@ const Footer = () => {
             <div>
               {/* Logo */}
               <Image
-                src="/images/logo.webp"
+                src="/images/logo1.svg"
                 alt="logo"
                 width={100}
                 height={100}
